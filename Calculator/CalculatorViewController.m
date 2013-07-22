707f847bbb69de09524297571fac5dd4b42ed4b9
Sheena Deutsch <sheena.deutsch@banno.com>
2013-07-22 15:16:06 -0500

    commit from Jamey

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -15,10 +15,11 @@
 
 //@property auto setter and getter
 // nonatomic means thread safe
-@property (nonatomic) BOOL UserIsInTheMiddleOfEnteringANumber;
-@property (nonatomic, strong) CalculatorBrain *brain;
+@property   (nonatomic)         BOOL    UserIsInTheMiddleOfEnteringANumber;
+@property   (nonatomic)         BOOL    UserIsInTheMiddleOfEnteringAFloat;
+@property   (nonatomic, strong)     CalculatorBrain *brain;
+@property   (nonatomic, weak) IBOutlet UILabel *display;
 @property (weak, nonatomic) IBOutlet UILabel *history;
-@property (weak, nonatomic) IBOutlet UILabel *display;
 
 @end
 
@@ -29,6 +30,7 @@
 //You don't want to name property and vaiable the same = bugs.
 @synthesize display = _display;
 @synthesize UserIsInTheMiddleOfEnteringANumber = _UserIsInTheMiddleOfEnteringANumber;
+@synthesize UserIsInTheMiddleOfEnteringAFloat = _UserIsInTheMiddleOfEnteringAFloat;
 @synthesize brain = _brain;
 @synthesize history = _history;
 
@@ -38,6 +40,16 @@
     if (!_brain) _brain = [[CalculatorBrain alloc] init];
     return _brain;
 }
+- (IBAction)dotPressed:(id)sender
+{
+    if(self.UserIsInTheMiddleOfEnteringAFloat)
+        return;
+    self.UserIsInTheMiddleOfEnteringANumber = YES;
+    self.UserIsInTheMiddleOfEnteringAFloat = YES;
+    self.display.text = [self.display.text stringByAppendingString:@"."];
+}
+
+
 //instance methods start with "-"
 - (IBAction)digitPressed:(UIButton *)sender
 {
@@ -51,26 +63,31 @@
     }
     self.history.text = self.history.text = [self.history.text stringByAppendingString:digit];
 }
-- (IBAction)operationPressed:(UIButton *)sender
-{
-    if (self.UserIsInTheMiddleOfEnteringANumber) [self enterPressed];
-    double result = [self.brain performOperation:sender.currentTitle];
-                                                  NSString *resultString = [NSString stringWithFormat:@"%g", result];
-                                                  self.display.text = resultString;
-}
+
 - (IBAction)enterPressed
 {
     [self.brain pushOperand:[self.display.text doubleValue]];
     self.UserIsInTheMiddleOfEnteringANumber = NO;
-    self.history.text = [self.history.text stringByAppendingString:@" "];
-    self.history.text = [self.history.text stringByAppendingString:self.display.text];
-    
+    //self.history.text = [self.history.text stringByAppendingString:@" "];
+    //self.history.text = [self.history.text stringByAppendingString:self.display.text];
 }
+
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if (self.UserIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    NSString *operation = [sender currentTitle];
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
+}
+
 
 - (IBAction)clearPressed:(UIButton *)sender
 {
     self.display.text = @"0";
-    self.history.text = @"0";
+    self.history.text = @"";
     self.UserIsInTheMiddleOfEnteringANumber = NO;
     [self.brain emptyStack];
 }
