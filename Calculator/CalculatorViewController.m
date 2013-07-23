16003fd0c6d30031ea560ae97bece65cdd204fe0
Sheena Deutsch <sheena.deutsch@banno.com>
2013-07-23 15:06:20 -0500

    extra credit almost done

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -15,86 +15,113 @@
 
 //@property auto setter and getter
 // nonatomic means thread safe
-@property   (nonatomic)         BOOL    UserIsInTheMiddleOfEnteringANumber;
-@property   (nonatomic)         BOOL    UserIsInTheMiddleOfEnteringAFloat;
-@property   (nonatomic, strong)     CalculatorBrain *brain;
-@property   (nonatomic, weak) IBOutlet UILabel *display;
-@property (weak, nonatomic) IBOutlet UILabel *history;
-
+@property   (nonatomic)         BOOL        userIsInTheMiddleOfEnteringANumber;
+@property   (nonatomic)         BOOL        userIsInTheMiddleOfEnteringAFloat;
+@property   (nonatomic)         BOOL        equalsIsShown;
+@property   (nonatomic, strong)             CalculatorBrain *brain;
+@property   (nonatomic, weak)   IBOutlet    UILabel *display;
+@property   (nonatomic, weak)   IBOutlet    UILabel *history;
+@property   (nonatomic)                     NSMutableArray *dotPressed;
 @end
 
 //definitions of methods after @implementation
 @implementation CalculatorViewController
 
-//@synthesize implements properties for us that we dont set in setter and getter.  
-//You don't want to name property and vaiable the same = bugs.
-@synthesize display = _display;
-@synthesize UserIsInTheMiddleOfEnteringANumber = _UserIsInTheMiddleOfEnteringANumber;
-@synthesize UserIsInTheMiddleOfEnteringAFloat = _UserIsInTheMiddleOfEnteringAFloat;
-@synthesize brain = _brain;
-@synthesize history = _history;
-
--(CalculatorBrain *)brain;
-{
+-(CalculatorBrain *)brain;{
+    //lazy instantiation: only loaded when needed.
     //alloc (allocation): heap allocation for a new object is done by the NSObject class method + (id)alloc.  It allocates enough space for all the instance variables.  init(initializing) classes can have multiple, different initalizers in addition to plain init.  both alloc and init must ahppen on right after the other.
     if (!_brain) _brain = [[CalculatorBrain alloc] init];
     return _brain;
 }
-- (IBAction)dotPressed:(id)sender
-{
-    if(self.UserIsInTheMiddleOfEnteringAFloat)
+
+
+- (IBAction)dotPressed:(id)sender{
+    if(!self.userIsInTheMiddleOfEnteringAFloat)
         return;
-    self.UserIsInTheMiddleOfEnteringANumber = YES;
-    self.UserIsInTheMiddleOfEnteringAFloat = YES;
+    self.userIsInTheMiddleOfEnteringANumber = YES;
+    self.userIsInTheMiddleOfEnteringAFloat = YES;
     self.display.text = [self.display.text stringByAppendingString:@"."];
 }
 
 
-//instance methods start with "-"
-- (IBAction)digitPressed:(UIButton *)sender
-{
+- (IBAction)digitPressed:(UIButton *)sender{
     //declaring a variable called "digit" and sending to sender??
     NSString *digit = sender.currentTitle;
-    if (self.UserIsInTheMiddleOfEnteringANumber) {
-    self.display.text = [self.display.text stringByAppendingString:digit];
-    }else{
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    }   else{
         self.display.text = digit;
         self.UserIsInTheMiddleOfEnteringANumber = YES;
     }
     self.history.text = self.history.text = [self.history.text stringByAppendingString:digit];
 }
 
-- (IBAction)enterPressed
-{
+
+- (IBAction)enterPressed {
     [self.brain pushOperand:[self.display.text doubleValue]];
-    self.UserIsInTheMiddleOfEnteringANumber = NO;
-    //self.history.text = [self.history.text stringByAppendingString:@" "];
-    //self.history.text = [self.history.text stringByAppendingString:self.display.text];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
 }
 
 
-- (IBAction)operationPressed:(UIButton *)sender
-{
-    if (self.UserIsInTheMiddleOfEnteringANumber) {
+- (IBAction)operationPressed:(UIButton *)sender{
+    if (self.userIsInTheMiddleOfEnteringANumber) {
         [self enterPressed];
     }
     NSString *operation = [sender currentTitle];
     double result = [self.brain performOperation:operation];
     self.display.text = [NSString stringWithFormat:@"%g", result];
+
+    self.history.text = [self.history.text  stringByAppendingString:@"="];
+    
+    self.equalsIsShown = YES;
 }
 
 
-- (IBAction)clearPressed:(UIButton *)sender
-{
-    self.display.text = @"0";
+- (IBAction)clearPressed:(UIButton *)sender{
+    self.display.text = @"";
     self.history.text = @"";
     self.UserIsInTheMiddleOfEnteringANumber = NO;
+    self.equalsIsShown = NO;
     [self.brain emptyStack];
 }
-- (IBAction)backspacePressed:(UIButton *)sender
-{
-    self.display.text = [self.display.text substringToIndex:[self.display.text length] -1];
-    self.history.text = [self.history.text substringToIndex:[self.history.text length] -1];
+
+
+- (IBAction)signChangedPressed:(UIButton *)sender {
+    self.display.text = [[NSNumber numberWithDouble:([self.display.text doubleValue]* -1)]stringValue];
 }
+
+- (IBAction)backspacePressed:(id)sender {
+    if(self.userIsInTheMiddleOfEnteringANumber){
+        if([self.display.text length] > 0) {
+            self.display.text = [self.display.text substringToIndex:[self.display.text length] -1];
+            self.history.text = [self.history.text substringToIndex:[self.history.text length] -1];
+        } else{
+            self.display.text = @"0";
+            self.userIsInTheMiddleOfEnteringANumber = NO;
+        }
+    }
+}
+
+
+- (IBAction)plusMinusPressed:(UIButton *)sender{
+    if(self.userIsInTheMiddleOfEnteringANumber){
+        if([[self.display.text substringToIndex:1]isEqualToString:@"-"]){
+            self.display.text = [self.display.text substringFromIndex:1];
+        } else {
+            self.display.text=[@"-" stringByAppendingString:self.display.text];
+        }
+    } else{
+        [self operationPressed:sender];
+    }
+    
+}
+//    if (self.userIsInTheMiddleOfEnteringANumber)
+//    {
+//        if([self.display.text hasPrefix:@"-"])
+//            self.display.text = [self.display.text substringFromIndex:1];
+//    } else self.display.text = [NSString stringWithFormat:@"-%@", self.display.text];
+//    return;
+//}
+
 
 @end
--- CalculatorBrain.m
+++ CalculatorBrain.m
@@ -9,91 +9,161 @@
 #import "CalculatorBrain.h"
 
 @interface CalculatorBrain()
-@property (nonatomic, strong) NSMutableArray *programStack;
+@property (nonatomic, strong) NSMutableArray *operandStack;
 @end
 
-@implementation CalculatorBrain
-@synthesize programStack = _programStack;
+@implementation CalculatorBrain;
+@synthesize operandStack = _operandStack;
 
--(NSMutableArray *) programStack;
-{
-    if (!_programStack) {
-        _programStack = [[NSMutableArray alloc] init];
+-(NSMutableArray *)operandStack{
+    if(!_operandStack){
+        _operandStack = [[NSMutableArray alloc] init];
     }
-    return _programStack;
+    return _operandStack;
 }
 
--(void)emptyStack
-{
-    _programStack = nil;
+-(void)emptyStack{
+    _operandStack = nil;
 }
 
-
--(void)pushOperand:(double)operand
-{
+-(void)pushOperand:(double)operand{
     NSNumber *operandObject = [NSNumber numberWithDouble:operand];
-    [self.programStack addObject:operandObject];
+    [self.operandStack addObject:operandObject];
 }
 
--(double)performOperation:(NSString *)operation
-{
-    [self.programStack addObject:operation];
-    return[CalculatorBrain runProgram:self.program];
+-(double)popOperand{
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if (operandObject) [self.operandStack removeLastObject];
+    return [operandObject doubleValue];
 }
 
--(id)program
-{
-    return [self.programStack copy];
-}
-
-+(NSString *)descriptionOfProgram:(id)program
-{
-    return @"Implement in assignment two @";
-}
-
-+(double)popOperandOffStack: (NSMutableArray *)stack
-{
+-(double)performOperation:(NSString *)operation{
     double result = 0;
     
-    id topOfStack = [stack lastObject];
-    if (topOfStack) [stack removeLastObject];
-    
-    if ([topOfStack isKindOfClass:[NSNumber class]]) {
-        result = [topOfStack doubleValue];
-    }else if ([topOfStack isKindOfClass:[NSString class]]) {
-        NSString *operation = topOfStack;
-        if ([operation isEqualToString:@"+"]) {
-            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
-        }else if ([@"*" isEqualToString:operation]){
-            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
-        }else if ([operation isEqualToString:@"-"]){
-            double subtrahend = [self popOperandOffStack:stack];
-            result = [self popOperandOffStack:stack] - subtrahend;
-        }else if ([operation isEqualToString:@"/"]) {
-            double divisor = [self popOperandOffStack:stack];
-            if (divisor) result = [self popOperandOffStack:stack] / divisor;
-        }else if ([operation isEqualToString:@"sin"]){
-            result = sin([self popOperandOffStack:stack]);
-        }else if ([operation isEqualToString:@"cos"]){
-            result = cos([self popOperandOffStack:stack]);
-        }else if ([operation isEqualToString:@"sqrt"]){
-            result = sqrt([self popOperandOffStack:stack]);
-        }else if ([operation isEqualToString:@"π"]){
-            result = [self popOperandOffStack:stack] * M_PI;
-        }
+    if ([operation isEqualToString:@"+"]) {
+        result = [self popOperand] + [self popOperand];
+    } else if ([@"*" isEqualToString:operation]){
+        result = [self popOperand] * [self popOperand];
+    } else if ([operation isEqualToString:@"-"]){
+        double subtrahend = [self popOperand];
+        result = [self popOperand] - subtrahend;
+    } else if ([operation isEqualToString:@"/"]) {
+        double divisor = [self popOperand];
+        if (divisor) result = [self popOperand] / divisor;
+    } else if ([operation isEqualToString:@"sin"]){
+        result = sin([self popOperand]);
+    } else if ([operation isEqualToString:@"cos"]){
+        result = cos([self popOperand]);
+    } else if ([operation isEqualToString:@"sqrt"]){
+        result = sqrt([self popOperand]);
+    } else if ([operation isEqualToString:@"π"]){
+        result = [self popOperand] * M_PI;
+    } else if ([operation isEqualToString:@"+/-"]){
+        result = [self popOperand] * -1;
     }
     
+    [self pushOperand:result];
     return result;
 }
 
-+(double)runProgram:(id)program
-{
-    NSMutableArray *stack;
-    if ([program isKindOfClass:[NSMutableArray class]]){
-        stack = [program mutableCopy];
-    }
-    return[self popOperandOffStack:stack];
-}
 
 
 @end
+
+
+
+
+
+
+//#import "CalculatorBrain.h"
+//
+//@interface CalculatorBrain()
+//@property (nonatomic, strong) NSMutableArray *programStack;
+//@end
+//
+//@implementation CalculatorBrain
+//@synthesize programStack = _programStack;
+//
+//-(NSMutableArray *) programStack;
+//{
+//    if (!_programStack) {
+//        _programStack = [[NSMutableArray alloc] init];
+//    }
+//    return _programStack;
+//}
+//
+//-(void)emptyStack
+//{
+//    _programStack = nil;
+//}
+//
+//
+//-(void)pushOperand:(double)operand
+//{
+//    NSNumber *operandObject = [NSNumber numberWithDouble:operand];
+//    [self.programStack addObject:operandObject];
+//}
+//
+//-(double)performOperation:(NSString *)operation
+//{
+//    [self.programStack addObject:operation];
+//    return[CalculatorBrain runProgram:self.program];
+//}
+//
+//-(id)program
+//{
+//    return [self.programStack copy];
+//}
+//
+//+(NSString *)descriptionOfProgram:(id)program
+//{
+//    return @"Implement in assignment two @";
+//}
+//
+//+(double)popOperandOffStack: (NSMutableArray *)stack
+//{
+//    double result = 0;
+//    
+//    id topOfStack = [stack lastObject];
+//    if (topOfStack) [stack removeLastObject];
+//    
+//    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+//        result = [topOfStack doubleValue];
+//    }else if ([topOfStack isKindOfClass:[NSString class]]) {
+//        NSString *operation = topOfStack;
+//        if ([operation isEqualToString:@"+"]) {
+//            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+//        } else if ([@"*" isEqualToString:operation]){
+//            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
+//        } else if ([operation isEqualToString:@"-"]){
+//            double subtrahend = [self popOperandOffStack:stack];
+//            result = [self popOperandOffStack:stack] - subtrahend;
+//        } else if ([operation isEqualToString:@"/"]) {
+//            double divisor = [self popOperandOffStack:stack];
+//            if (divisor) result = [self popOperandOffStack:stack] / divisor;
+//        } else if ([operation isEqualToString:@"sin"]){
+//            result = sin([self popOperandOffStack:stack]);
+//        } else if ([operation isEqualToString:@"cos"]){
+//            result = cos([self popOperandOffStack:stack]);
+//        } else if ([operation isEqualToString:@"sqrt"]){
+//            result = sqrt([self popOperandOffStack:stack]);
+//        } else if ([operation isEqualToString:@"π"]){
+//            result = [self popOperandOffStack:stack] * M_PI;
+//        } else if ([operation isEqualToString:@"+/="]){
+//            result = [self popOperandOffStack:stack] * -1;
+//        }
+//    }
+//    
+//    return result;
+//}
+//
+//+(double)runProgram:(id)program
+//{
+//    NSMutableArray *stack;
+//    if ([program isKindOfClass:[NSMutableArray class]]){
+//        stack = [program mutableCopy];
+//    }
+//    return[self popOperandOffStack:stack];
+//}
+//
+
